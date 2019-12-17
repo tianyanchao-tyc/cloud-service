d6764a9b3e52eb6990a2fd73e2386b6c18df2e07
syxy_chengzihao <tianyanchao>
2019-12-17 16:39:15 +0800

    微服务注册中心

---
+++ src/main/java/base/service/cloud/CloudServiceApplication.java
@@ -0,0 +1,15 @@
+package base.service.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class CloudServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudServiceApplication.class, args);
+    }
+
+}
