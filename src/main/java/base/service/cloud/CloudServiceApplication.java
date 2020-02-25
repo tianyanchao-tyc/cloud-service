391d4e7c066a67af962f99834221db1bd9e92dfc
syxy_chengzihao <tianyanchao>
2020-02-25 11:35:37 +0800

    微服务注册中心

--- src/main/java/base/service/cloud/CloudServiceApplication.java
+++ src/main/java/base/service/cloud/CloudServiceApplication.java
@@ -2,9 +2,11 @@ package base.service.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @EnableEurekaServer
 public class CloudServiceApplication {
 
